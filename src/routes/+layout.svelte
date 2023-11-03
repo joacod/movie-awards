645ccffd07dd9c364f4558254eb8444feb09b345
Joaquin Diaz <joacod@gmail.com>
2023-11-03 19:18:02 -0300

    search details

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,13 @@
 <script>
-import "../app.css";
+	import '../app.css';
 </script>
 
+<svelte:head>
+	<title>Movie Awards</title>
+</svelte:head>
+
 <div class="flex flex-col items-center justify-center min-h-screen bg-[#222831]">
-    <div class="w-full max-w-3xl p-4 text-[#eeeeee]">
-        <slot />
-    </div>
+	<div class="w-full max-w-3xl p-4 text-[#eeeeee]">
+		<slot />
+	</div>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,48 +1,73 @@
 <script>
-    let data = null;
-    let name = '';
-    let results = [];
-    
-    async function getMovieIdsByName() {
-        const response = await fetch(`/api/movies/${encodeURIComponent(name)}`);
-        data = await response.json();
-        results = data.results;
-    };
+	import { onMount } from 'svelte';
+
+	let inputElement;
+	let data = null;
+	let name = '';
+	let searchedName = '';
+	let results = [];
+	let loading = false;
+
+	onMount(() => {
+		inputElement.focus();
+	});
+
+	async function getMovieIdsByName() {
+		loading = true;
+		searchedName = name;
+		const response = await fetch(`/api/movies/${encodeURIComponent(name)}`);
+		data = await response.json();
+		results = data.results;
+		loading = false;
+		name = '';
+	}
+
+	async function handleFormSubmit() {
+		await getMovieIdsByName();
+		inputElement.focus();
+	}
 </script>
 
 <h1 class="text-4xl font-semibold text-center mb-6">Movie Awards</h1>
 
-<div class="mb-8 flex items-center justify-center">
-    <input
-        bind:value={name}
-        type="search"
-        class="flex h-10 rounded-md border bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 w-1/2 mr-2 border-[#393e46] text-[#222831] focus:border-[#00adb5]"
-        placeholder="Search movie..."
-    />
-    <button
-        on:click={getMovieIdsByName}
-        class="inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border bg-background h-10 px-4 py-2 border-[#00adb5] text-[#00adb5] hover:bg-[#00adb5] hover:text-[#222831]"
-        type="submit"
-    >
-        <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        class=" h-5 w-5"
-        >
-        <circle cx="11" cy="11" r="8"></circle>
-        <path d="m21 21-4.3-4.3"></path>
-        </svg>
-    </button>
-</div>
+<form on:submit|preventDefault={handleFormSubmit} class="mb-8 flex items-center justify-center">
+	<input
+		bind:this={inputElement}
+		bind:value={name}
+		type="search"
+		class="flex h-10 rounded-md border bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 w-1/2 mr-2 border-[#393e46] text-[#222831] focus:border-[#00adb5]"
+		placeholder="Enter movie name"
+	/>
+	<button
+		disabled={loading}
+		class="inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border bg-background h-10 px-4 py-2 border-[#00adb5] text-[#00adb5] hover:bg-[#00adb5] hover:text-[#222831]"
+		type="submit"
+	>
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			width="24"
+			height="24"
+			viewBox="0 0 24 24"
+			fill="none"
+			stroke="currentColor"
+			stroke-width="2"
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			class=" h-5 w-5"
+		>
+			<circle cx="11" cy="11" r="8" />
+			<path d="m21 21-4.3-4.3" />
+		</svg>
+	</button>
+</form>
 
-{#each results as movie}
-    <h3>{movie.imdb_id}</h3>
-    <p>{movie.title}</p>
-{/each}
+{#if loading}
+	<p>Loading...</p>
+{:else if searchedName !== '' && results.length === 0}
+	<p>No movies found with the name "{searchedName}"</p>
+{:else}
+	{#each results as movie}
+		<h3>{movie.imdb_id}</h3>
+		<p>{movie.title}</p>
+	{/each}
+{/if}
