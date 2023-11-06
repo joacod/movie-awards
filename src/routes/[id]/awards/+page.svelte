3e0806ca8a59004b75eccc45d13c7900cf51b325
Joaquin Diaz <joacod@gmail.com>
2023-11-05 22:00:21 -0300

    awards table

--- src/routes/[id]/awards/+page.svelte
+++ src/routes/[id]/awards/+page.svelte
@@ -8,6 +8,7 @@
 	let winners = [];
 	let nominees = [];
 	let loading = true;
+	let activeTable = 'winner';
 
 	onMount(async () => {
 		const response = await fetch(`/api/movies/${data.movieId}/awards`);
@@ -46,8 +47,8 @@
 	<title>Movie Awards - {data.movieName}</title>
 </svelte:head>
 
-<h1 class="text-4xl font-semibold text-center mb-2">{data.movieName}</h1>
-<h2 class="text-2xl font-semibold text-center mb-6">Awards</h2>
+<h1 class="text-4xl font-semibold text-center mb-5">Awards</h1>
+<h2 class="text-2xl font-semibold text-center mb-10">{data.movieName}</h2>
 
 {#if loading}
 	<div class="flex justify-center">
@@ -56,33 +57,67 @@
 {:else if awards && awards.results.length === 0}
 	<p class="text-center">No awards found.</p>
 {:else}
-	<div class="flex justify-center content-center">
-		<div>
-			<h3 class="text-xl">Winner</h3>
-			<ul>
-				{#each winners as item}
-					<li class="mt-4">
-						<p>Award Name: {item.award_name}</p>
-						<p>Year: {item.year}</p>
-						<p>Category: {item.award}</p>
-						<p>Event: {item.event_name}</p>
-					</li>
-				{/each}
-			</ul>
+	<div class="flex flex-col items-center justify-center">
+		<div class="flex justify-center space-x-5 mb-5">
+			<button
+				class={`${
+					activeTable === 'winner' ? 'bg-[#00adb5] text-white' : 'bg-background'
+				} inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border h-10 px-4 py-2 border-[#00adb5] text-[#00adb5] hover:bg-[#00adb5] hover:text-white`}
+				on:click={() => (activeTable = 'winner')}>Winner</button
+			>
+			<button
+				class={`${
+					activeTable === 'nominee' ? 'bg-[#00adb5] text-white' : 'bg-background'
+				} inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border h-10 px-4 py-2 border-[#00adb5] text-[#00adb5] hover:bg-[#00adb5] hover:text-white`}
+				on:click={() => (activeTable = 'nominee')}>Nominee</button
+			>
 		</div>
-
-		<div>
-			<h3 class="text-xl">Nominee</h3>
-			<ul>
-				{#each nominees as item}
-					<li class="mt-4">
-						<p>Award Name: {item.award_name}</p>
-						<p>Year: {item.year}</p>
-						<p>Category: {item.award}</p>
-						<p>Event: {item.event_name}</p>
-					</li>
-				{/each}
-			</ul>
+		<div class="flex flex-col mt-4">
+			<div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+				<div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+					<div class="shadow overflow-hidden sm:rounded-lg">
+						<table class="min-w-full text-sm text-gray-400">
+							<thead class="bg-[#00adb5] text-white text-sm uppercase font-medium">
+								<tr>
+									<th scope="col" class="px-6 py-3 text-left tracking-wider">Award Name</th>
+									<th scope="col" class="px-6 py-3 text-left tracking-wider">Year</th>
+									<th scope="col" class="px-6 py-3 text-left tracking-wider">Category</th>
+									<th scope="col" class="px-6 py-3 text-left tracking-wider">Event</th>
+								</tr>
+							</thead>
+							<tbody class="bg-gray-800">
+								{#if activeTable === 'winner'}
+									{#each winners as item, i}
+										<tr
+											class:bg-black={i % 2 === 0}
+											class:bg-opacity-20={i % 2 === 0}
+											class:bg-transparent={i % 2 !== 0}
+										>
+											<td class="px-6 py-4 whitespace-nowrap">{item.award_name}</td>
+											<td class="px-6 py-4 whitespace-nowrap">{item.year}</td>
+											<td class="px-6 py-4 whitespace-nowrap">{item.award}</td>
+											<td class="px-6 py-4 whitespace-nowrap">{item.event_name}</td>
+										</tr>
+									{/each}
+								{:else if activeTable === 'nominee'}
+									{#each nominees as item, i}
+										<tr
+											class:bg-black={i % 2 === 0}
+											class:bg-opacity-20={i % 2 === 0}
+											class:bg-transparent={i % 2 !== 0}
+										>
+											<td class="px-6 py-4 whitespace-nowrap">{item.award_name}</td>
+											<td class="px-6 py-4 whitespace-nowrap">{item.year}</td>
+											<td class="px-6 py-4 whitespace-nowrap">{item.award}</td>
+											<td class="px-6 py-4 whitespace-nowrap">{item.event_name}</td>
+										</tr>
+									{/each}
+								{/if}
+							</tbody>
+						</table>
+					</div>
+				</div>
+			</div>
 		</div>
 	</div>
 {/if}
