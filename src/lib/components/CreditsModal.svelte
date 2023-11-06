7f79ec424c541a7f2e8f70351f7682da53661dce
Joaquin Diaz <joacod@gmail.com>
2023-11-06 19:02:16 -0300

    credits modal

---
+++ src/lib/components/CreditsModal.svelte
@@ -0,0 +1,65 @@
+<script>
+	export let toggleModal;
+</script>
+
+<div
+	class="absolute top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-50"
+>
+	<div class="bg-white p-4 text-sm rounded flex flex-col items-center justify-center">
+		<h2 class="mb-5 text-lg font-bold">Credits</h2>
+		<div class="mb-3">
+			<p>
+				<a
+					class="underline"
+					href="https://github.com/joacod/movie-awards"
+					target="_blank"
+					rel="noopener noreferrer"
+					title="Movie Awards on GitHub"
+				>
+					Movie Awards
+				</a>{' '}
+				created by{' '}
+				<a
+					class="underline"
+					href="https://github.com/joacod"
+					target="_blank"
+					rel="noopener noreferrer"
+					title="Joaquin Diaz on GitHub"
+				>
+					Joaquin Diaz
+				</a>
+			</p>
+		</div>
+		<ul>
+			<li>
+				Favicon from
+				<a
+					class="underline"
+					href="https://icons8.com/icons/set/clapper"
+					target="_blank"
+					rel="noopener noreferrer"
+					title="Shots.so website"
+				>
+					icons8.com
+				</a>
+			</li>
+			<li>
+				Movies api
+				<a
+					class="underline"
+					href="https://rapidapi.com/SAdrian/api/moviesminidatabase/"
+					target="_blank"
+					rel="noopener noreferrer"
+					title="UAParser website"
+				>
+					rapidapi.com
+				</a>
+			</li>
+		</ul>
+
+		<button
+			class="mt-6 bg-[#00adb5] text-white rounded px-4 py-2 hover:bg-[#068c92]"
+			on:click={toggleModal}>Close</button
+		>
+	</div>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,26 @@
 <script>
 	import '../app.css';
+	import CreditsModal from '../lib/components/CreditsModal.svelte';
+	let showModal = false;
+
+	function toggleModal() {
+		showModal = !showModal;
+	}
 </script>
 
 <svelte:head>
 	<title>Movie Awards</title>
 </svelte:head>
 
-<div class="flex flex-col items-center justify-center min-h-screen">
+<div class="flex flex-col items-center justify-center min-h-screen relative">
+	<button
+		class="absolute top-4 right-4 bg-[#00adb5] text-white text-sm rounded px-3 py-1 hover:bg-[#068c92]"
+		on:click={() => (showModal = true)}>Credits</button
+	>
 	<div class="w-full px-40 py-10 text-[#eeeeee]">
 		<slot />
 	</div>
+	{#if showModal}
+		<CreditsModal {toggleModal} />
+	{/if}
 </div>
