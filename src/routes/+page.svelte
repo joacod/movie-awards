3ec70c91c92174f4a408972aff9b1231ab2dc90b
Joaquin Diaz <joacod@gmail.com>
2023-11-06 19:33:17 -0300

    icons added

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -40,7 +40,29 @@
 	<title>Movie Awards</title>
 </svelte:head>
 
-<h1 class="text-5xl font-semibold text-center mb-6">Movie Awards</h1>
+<div class="flex justify-center items-center mb-6">
+	<svg
+		width="40"
+		height="40"
+		viewBox="0 0 24 24"
+		stroke-width="1.5"
+		stroke="currentColor"
+		fill="none"
+		stroke-linecap="round"
+		stroke-linejoin="round"
+	>
+		<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+		<path d="M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z" />
+		<path d="M8 4l0 16" />
+		<path d="M16 4l0 16" />
+		<path d="M4 8l4 0" />
+		<path d="M4 16l4 0" />
+		<path d="M4 12l16 0" />
+		<path d="M16 8l4 0" />
+		<path d="M16 16l4 0" />
+	</svg>
+	<h1 class="text-5xl font-semibold text-center ml-2">Movie Awards</h1>
+</div>
 
 <form on:submit|preventDefault={handleFormSubmit} class="my-10 flex items-center justify-center">
 	<input
--- src/routes/[id]/awards/+page.svelte
+++ src/routes/[id]/awards/+page.svelte
@@ -47,7 +47,36 @@
 	<title>Movie Awards - {data.movieName}</title>
 </svelte:head>
 
-<h1 class="text-4xl font-semibold text-center mb-5">Awards</h1>
+<div class="flex justify-center items-center mb-5">
+	<svg
+		width="40"
+		height="40"
+		viewBox="0 0 24 24"
+		stroke-width="1.5"
+		stroke="currentColor"
+		fill="none"
+		stroke-linecap="round"
+		stroke-linejoin="round"
+	>
+		<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+		<path
+			d="M19.496 13.983l1.966 3.406a1.001 1.001 0 0 1 -.705 1.488l-.113 .011l-.112 -.001l-2.933 -.19l-1.303 2.636a1.001 1.001 0 0 1 -1.608 .26l-.082 -.094l-.072 -.11l-1.968 -3.407a8.994 8.994 0 0 0 6.93 -3.999z"
+			stroke-width="0"
+			fill="currentColor"
+		/>
+		<path
+			d="M11.43 17.982l-1.966 3.408a1.001 1.001 0 0 1 -1.622 .157l-.076 -.1l-.064 -.114l-1.304 -2.635l-2.931 .19a1.001 1.001 0 0 1 -1.022 -1.29l.04 -.107l.05 -.1l1.968 -3.409a8.994 8.994 0 0 0 6.927 4.001z"
+			stroke-width="0"
+			fill="currentColor"
+		/>
+		<path
+			d="M12 2l.24 .004a7 7 0 0 1 6.76 6.996l-.003 .193l-.007 .192l-.018 .245l-.026 .242l-.024 .178a6.985 6.985 0 0 1 -.317 1.268l-.116 .308l-.153 .348a7.001 7.001 0 0 1 -12.688 -.028l-.13 -.297l-.052 -.133l-.08 -.217l-.095 -.294a6.96 6.96 0 0 1 -.093 -.344l-.06 -.271l-.049 -.271l-.02 -.139l-.039 -.323l-.024 -.365l-.006 -.292a7 7 0 0 1 6.76 -6.996l.24 -.004z"
+			stroke-width="0"
+			fill="currentColor"
+		/>
+	</svg>
+	<h1 class="text-4xl font-semibold text-center ml-2">Awards</h1>
+</div>
 <h2 class="text-2xl font-semibold text-center mb-10">{data.movieName}</h2>
 
 {#if loading}
@@ -63,7 +92,28 @@
 				class={`${
 					activeTable === 'winner' ? 'bg-[#00adb5] text-white' : 'bg-background'
 				} inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border h-10 px-4 py-2 border-[#00adb5] text-[#00adb5] hover:bg-[#00adb5] hover:text-white`}
-				on:click={() => (activeTable = 'winner')}>Winner</button
+				on:click={() => (activeTable = 'winner')}
+			>
+				<svg
+					class="mr-1"
+					width="24"
+					height="24"
+					viewBox="0 0 24 24"
+					stroke-width="1.25"
+					stroke="currentColor"
+					fill="none"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+				>
+					<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+					<path d="M8 21l8 0" />
+					<path d="M12 17l0 4" />
+					<path d="M7 4l10 0" />
+					<path d="M17 4v8a5 5 0 0 1 -10 0v-8" />
+					<path d="M5 9m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
+					<path d="M19 9m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
+				</svg>
+				Winner</button
 			>
 			<button
 				class={`${
